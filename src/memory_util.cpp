c8812d99d5bee6f1298cad022e194eed7f847eb3
h00679994 <hejinfeng5@huawei.com>
2025-07-29 11:15:00 +0800

    删除废弃文件

--- src/memory_util.cpp
+++
@@ -1,8 +0,0 @@
-// 内存操作工具
-
-#include <sys/ptrace.h>
-
-uintptr_t alloc_mem_in_proc()
-{
-   
-}
--- src/patch_replace.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-void __attribute__((constructor)) init_func() {
-    printf("replace func\n");
-}
